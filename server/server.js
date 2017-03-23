184e0ec094131c11e307cb026727c331df2b070e
bkdev98 <bkdev98@gmail.com>
2017-03-23 09:31:14 +0700

    Separate firebase config to JSON file

--- server/server.js
+++ server/server.js
@@ -14,13 +14,7 @@ MomentHandler.registerHelpers(hbs);
 
 const firebase = require('firebase');
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDt6RXCwFDaOiAT8QhZyU3iOKGrAiRPxNU",
-    authDomain: "messenger-app-6fb8e.firebaseapp.com",
-    databaseURL: "https://messenger-app-6fb8e.firebaseio.com",
-    storageBucket: "messenger-app-6fb8e.appspot.com",
-    messagingSenderId: "250437655467"
-};
+const firebaseConfig = require('./config/firebase.json');
 
 firebase.initializeApp(firebaseConfig);
 
